9df3c527ace3dcb11e2cda899fd8cdeb777e7795
Jiale-Wu <Jwu45@Charlotte.edu>
2025-11-30 18:57:12 -0500

    Update SignupInstead.java

--- app/src/main/java/com/example/capstoneproject/SignupInstead.java
+++ app/src/main/java/com/example/capstoneproject/SignupInstead.java
@@ -1,12 +1,18 @@
 package com.example.capstoneproject;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.capstoneproject.databinding.FragmentProfilePictureBinding;
+import com.example.capstoneproject.databinding.FragmentSignupInsteadBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +25,7 @@ public class SignupInstead extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    FragmentSignupInsteadBinding binding;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -59,6 +66,29 @@ public class SignupInstead extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup_instead, container, false);
+        binding = FragmentSignupInsteadBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.backbutton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.back();
+            }
+        });
+    }
+    SignupInsteadListener mListener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mListener = (SignupInsteadListener) context;
+    }
+
+    interface SignupInsteadListener {
+        void back();
     }
 }
